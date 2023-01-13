9c7e2d5d98065041193a34f6f69a00873f8e6476
Stepan Taloverenko <taloverenko@gmail.com>
2023-01-13 15:45:42 +0400

    object ref

--- 06_objects/07_object-ref.js
+++ 06_objects/07_object-ref.js
@@ -3,13 +3,19 @@ let otherDigit = digit;
 // Исходное значение не меняется, т.к. otherDigit копирует значение из digit
 otherDigit += 5; // digit = 20, otherDigit = 25
 
+console.log( digit );
+console.log( otherDigit );
 
 // Объекты не копируют значения, а ссылаются на одно место, в котором находятся значения
-let obj = { model: 'VW Polo'};
+let obj = { model: 'VW Polo' };
 let otherObj = obj;
 otherObj.model = 'Volkswagen Polo' // Значение заменится в обоих объектах
 
-console.log( digit );
-console.log( otherDigit );
 console.log( obj );
 console.log( otherObj );
+console.log(obj === otherObj ); // true
+
+otherObj = { model: 'VW Polo' };
+console.log( obj );
+console.log( otherObj );
+console.log(obj === otherObj ); // false
