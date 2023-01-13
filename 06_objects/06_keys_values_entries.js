9615fb1f4fb58e09b9cae45e9e4193a333af3b99
Stepan Taloverenko <taloverenko@gmail.com>
2023-01-13 15:09:54 +0400

    keys values

---
+++ 06_objects/06_keys_values_entries.js
@@ -0,0 +1,17 @@
+let computer = {
+  price: 150000,
+  usbPortCount: 8,
+  chipset: 'AMD X570',
+  socket: 'AM4',
+  coreCount: 8,
+  cpuManufacturer: 'AMD',
+  videocardModel: 'NVidia GeForce GTX 1650 super',
+  videoMemory: 4096,
+  ramType: 'DDR4',
+  ramVolume: 16384,
+  remFrequency: 3200,
+};
+
+console.log( Object.keys(computer) ); // Список имён свойств объекта
+console.log( Object.values(computer) ); // Список значений свойств объекта
+console.log( Object.entries(computer) ); // Список и того и другого
